bdde3cc605a9bbefb328195807762e65055d77ac
amarmouhamed-IA <mouhamed.amar1401@gmail.com>
2025-04-05 21:41:47 +0000

    page register et Dashboard professeur

--- plateforme-evaluation-bdd/frontend/src/pages/CorrectionModelsPage.jsx
+++ plateforme-evaluation-bdd/frontend/src/pages/CorrectionModelsPage.jsx
@@ -183,7 +183,7 @@ const CorrectionModelsPage = () => {
 
             {showAddModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                    <div className="bg-gray-800 rounded-xl p-6 w-full max-w-3xl">
+                    <div className="bg-gray-800 rounded-xl p-6 w-full max-w-4xl">
                         <div className="flex justify-between items-center mb-4">
                             <h2 className="text-xl font-semibold">
                                 {selectedExercise && selectedExercise.id
